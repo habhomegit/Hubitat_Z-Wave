b33aac8c3c3699faccd46f66653b09f03ec52dea
habhomegit <42719976+habhomegit@users.noreply.github.com>
2021-07-30 16:22:45 -0500

    Update iblindsV3_10.groovy

--- iblindsV3_10.groovy
+++ iblindsV3_10.groovy
@@ -28,7 +28,7 @@ metadata {
 	//	fingerprint inClusters: "0x26"
    	fingerprint type: "1106", cc: "5E,85,59,86,72,5A,73,26,25,80"
 	fingerprint mfr:"0287", prod:"0004", model:"0071", deviceJoinName: "iBlinds V3"
-    fingerprint mfr:"0287", prod:"0004", model:"0072", deviceJoinName: "iBlinds V3.10+"
+        fingerprint mfr:"0287", prod:"0004", model:"0072", deviceJoinName: "iBlinds V3.1"
 
 	simulator {
 		status "on":  "command: 2003, payload: FF"
