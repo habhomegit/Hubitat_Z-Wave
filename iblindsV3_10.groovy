b14bb49f281274eb598a0d9e8af07fbdfa66de27
Gassgs <71162352+Gassgs@users.noreply.github.com>
2021-10-07 10:28:05 -0500

    Add Position attribute updates, added option to turn off logging
    Add Position attribute updates, added option to turn off logging

--- iblindsV3_10.groovy
+++ iblindsV3_10.groovy
@@ -11,8 +11,9 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  *		Written by: Chance H, 6-26-20
- *      Update Eric B, 08-14-20 - Fix Configuration and Set Level/Set Position
+ *      	Update Eric B, 08-14-20 - Fix Configuration and Set Level/Set Position
  *		Update Chance H, 06-11-21 - Add Fingerprint for V3.10+, Add Parameters 7 - 11 for V3.06+
+ *		Update Gassgs, 10-06-21 - Add Position attribute updates, added option to turn off logging
  *
  */
 metadata {
@@ -20,9 +21,9 @@ metadata {
 		capability "Switch Level"
 		capability "Actuator"
 		capability "Switch"
-        capability "Window Shade"   
+        	capability "Window Shade"   
 		capability "Refresh"
-        capability "Battery"
+        	capability "Battery"
    
 
 	//	fingerprint inClusters: "0x26"
@@ -47,42 +48,6 @@ metadata {
 		reply "20014B,delay 5000,2602": "command: 2603, payload: 4B"
 		reply "200163,delay 5000,2602": "command: 2603, payload: 63"
 	}
-
-	tiles(scale: 2) {
-		multiAttributeTile(name:"blind", type: "lighting", width: 6, height: 4, canChangeIcon: true, canChangeBackground: true){
-			tileAttribute ("device.windowShade", key: "PRIMARY_CONTROL") {
-				attributeState "open", label:'${name}', action:"switch.off", icon:"https://raw.githubusercontent.com/habhomegit/Smartthings_Z-Wave/master/blind.png", backgroundColor:"#00B200", nextState:"closing"
-				attributeState "closed", label:'${name}', action:"switch.on", icon:"https://raw.githubusercontent.com/habhomegit/Smartthings_Z-Wave/master/blind.png", backgroundColor:"#ffffff", nextState:"opening"
-				attributeState "opening", label:'${name}', action:"switch.off", icon:"https://raw.githubusercontent.com/habhomegit/Smartthings_Z-Wave/master/blind.png", backgroundColor:"#00B200", nextState:"closing"
-				attributeState "closing", label:'${name}', action:"switch.on", icon:"https://raw.githubusercontent.com/habhomegit/Smartthings_Z-Wave/master/blind.png", backgroundColor:"#ffffff", nextState:"opening" 
-			}
-            
-            tileAttribute ("device.level", key: "SLIDER_CONTROL") {
-			attributeState "level", action:"switch level.setLevel"
-		}
-            tileAttribute("device.battery", key: "SECONDARY_CONTROL") {
-            attributeState "battery", label:'Battery Level: ${currentValue}%', unit:"%"    
-            }
-	
-		}
-
-		valueTile("battery", "device.battery", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {	
-        	state "battery", label:'${currentValue}% Battery Level', unit:""
-		}
-        
-        valueTile("levelval", "device.level", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {	
-        	state "Level", label:'${currentValue}% Tilt Angle', unit:""
-		}
-        
-		standardTile("refresh", "device.switch", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
-			state "default", label:"Refresh", action:"refresh.refresh", icon:"st.secondary.refresh"
-		}
-		}
-
-		main(["blind"])
-       	details(["blind", "levelval", "battery", "levelSliderControl",  "refresh"])
-
-	}
     
       preferences {
         
@@ -93,18 +58,20 @@ metadata {
         input name: "NVM_Target_Value",type: "number", title: "Default ON Value",defaultValue: 50, range: "1..100",  description: "Used to set the default ON level when manual push button is pushed",required: true, displayDuringSetup:false
         input name: "NVM_Device_Reset_Support",type: "bool",title: "Disable Reset Button", description: "Used for situations where the buttons are being held down accidentally via a tight space, etc.", defaultValue: false
         input name: "Speed_Parameter",type: "number",title: "Open/Close Speed(seconds)", 	defaultValue: 0, range:"0..100",	description: "To slow down the blinds, increase the value",required: true, displayDuringSetup: false
-		input name: "Init_Calib", type: "bool", title: "Initiate Calibration", defaultValue: false, description: "Will begin calibration after the next command is sent (V3.06+). Change to false when complete", displayDuringSetup: false
-		input name: "MinTilt",type: "number", title: "Lower close value",defaultValue: 0, range: "0..25",  description: "Increase if lower interval is closing too tightly.",required: true, displayDuringSetup:false
-		input name: "MaxTilt",type: "number", title: "Upper close value",defaultValue: 100, range: "75..100",  description: "Increase if upper interval is closing too tightly.",required: true, displayDuringSetup:false
-		input name: "ReMap",type: "bool", title: "Re-map to 0x63",defaultValue: false, description:"Not applicable to Hubitat",required: true, displayDuringSetup:false
-		input name: "MultiChange",type: "bool", title: "Allow MultiLevelStopChange",defaultValue: false,  description: "Allows use of MultiLevelStopChange",required: true, displayDuringSetup:false
- 	}
-    
+	input name: "Init_Calib", type: "bool", title: "Initiate Calibration", defaultValue: false, description: "Will begin calibration after the next command is sent (V3.06+). Change to false when complete", displayDuringSetup: false
+	input name: "MinTilt",type: "number", title: "Lower close value",defaultValue: 0, range: "0..25",  description: "Increase if lower interval is closing too tightly.",required: true, displayDuringSetup:false
+	input name: "MaxTilt",type: "number", title: "Upper close value",defaultValue: 100, range: "75..100",  description: "Increase if upper interval is closing too tightly.",required: true, displayDuringSetup:false
+	//input name: "ReMap",type: "bool", title: "Re-map to 0x63",defaultValue: false, description:"Not applicable to Hubitat",required: true, displayDuringSetup:false
+	input name: "MultiChange",type: "bool", title: "Allow MultiLevelStopChange",defaultValue: false,  description: "Allows use of MultiLevelStopChange",required: true, displayDuringSetup:false
+        input name: "logEnable",type: "bool", title: "Enable debug logging",defaultValue: false,required: true
+      }
+ 	}       
 }
+    
 def parse(String description) {
 	def result = null
 	if (description != "updated") {
-		log.debug "parse() >> zwave.parse($description)"
+		if (logEnable) log.debug "parse() >> zwave.parse($description)"
 		def cmd = zwave.parse(description, [0x20: 1, 0x26: 1, 0x70: 1])
 		if (cmd) {
 			result = zwaveEvent(cmd)
@@ -112,9 +79,9 @@ def parse(String description) {
 	}
 	if (result?.name == 'hail' && hubFirmwareLessThan("000.011.00602")) {
 		result = [result, response(zwave.basicV1.basicGet())]
-		log.debug "Was hailed: requesting state update"
+		if (logEnable) log.debug "Was hailed: requesting state update"
 	} else {
-		log.debug "Parse returned ${result?.descriptionText}"
+		if (logEnable) log.debug "Parse returned ${result?.descriptionText}"
 	}
 	return result
 }
@@ -138,12 +105,14 @@ def zwaveEvent(hubitat.zwave.commands.switchmultilevelv1.SwitchMultilevelSet cmd
 }
 
 private dimmerEvents(hubitat.zwave.Command cmd) {
+    if (logEnable) log.debug "$cmd"
 	def value = (cmd.value ? "on" : "off")
 	def result = [createEvent(name: "switch", value: value)]
 	if (cmd.value && cmd.value <= 100) {
-		result << createEvent(name: "level", value: cmd.value, unit: "%")
+		sendEvent(name: "level", value: cmd.value, unit: "%")
+        sendEvent(name: "position", value: cmd.value, unit: "%")
 	}
-	return result
+    if (logEnable) log.debug "$value"
 }
 
 def zwaveEvent(hubitat.zwave.commands.hailv1.Hail cmd) {
@@ -151,10 +120,10 @@ def zwaveEvent(hubitat.zwave.commands.hailv1.Hail cmd) {
 }
 
 def zwaveEvent(hubitat.zwave.commands.manufacturerspecificv2.ManufacturerSpecificReport cmd) {
-	log.debug "manufacturerId:   ${cmd.manufacturerId}"
-	log.debug "manufacturerName: ${cmd.manufacturerName}"
-	log.debug "productId:        ${cmd.productId}"
-	log.debug "productTypeId:    ${cmd.productTypeId}"
+	if (logEnable) log.debug "manufacturerId:   ${cmd.manufacturerId}"
+	if (logEnable) log.debug "manufacturerName: ${cmd.manufacturerName}"
+	if (logEnable) log.debug "productId:        ${cmd.productId}"
+	if (logEnable) log.debug "productTypeId:    ${cmd.productTypeId}"
 	def msr = String.format("%04X-%04X-%04X", cmd.manufacturerId, cmd.productTypeId, cmd.productId)
 	updateDataValue("MSR", msr)
 	createEvent([descriptionText: "$device.displayName MSR: $msr", isStateChange: false])
@@ -170,20 +139,19 @@ def zwaveEvent(hubitat.zwave.Command cmd) {
 }
 
 def on() {
-		open()
+	open()
 }
 
 def off() {
-		close()
+	close()
 }
 
 def setPosition(value) {
-    log.trace "presetPosition()"
     setLevel(value)
 }
 
 def setLevel(value, duration=0) {
-    log.debug "setLevel >> value: $value, duration: $duration"
+    if (logEnable) log.debug "setLevel >> value: $value, duration: $duration"
     def level = Math.max(Math.min(value as Integer, 99), 0)
 
     if (level <= 0 || level >= 99) {
@@ -195,6 +163,7 @@ def setLevel(value, duration=0) {
     }
 
     sendEvent(name: "level", value: level, unit: "%")
+    sendEvent(name: "position", value: level, unit: "%")
     def setLevel = reverse ? 99 - level : level
     def dimmingDuration = duration < 128 ? duration : 128 + Math.round(duration / 60)
     zwave.switchMultilevelV2.switchMultilevelSet(value: setLevel, dimmingDuration: dimmingDuration).format()
@@ -217,7 +186,7 @@ def zwaveEvent(hubitat.zwave.commands.batteryv1.BatteryReport cmd) {
 
 def installed () {
     // When device is installed get battery level and set daily schedule for battery refresh
-    log.debug "Installed..."
+    if (logEnable) log.debug "Installed..."
     runIn(15,getBattery) 
     schedule("$time",getBattery)
     
@@ -225,35 +194,51 @@ def installed () {
 
 def updated () {
     // When device is updated get battery level and set daily schedule for battery refresh
-    log.debug "Updated..."
+    if (logEnable) log.debug "Updated..."
     runIn(15,getBattery) 
     schedule("$time",getBattery)
-    log.debug "Call Update Params"
+    if (logEnable) log.debug "Call Update Params"
     configureParams()
     //configureParams()
 }
 
 def open() {
-	log.debug "open()"
+	if (logEnable) log.debug "open()"
 	// Blinds fully open at 50%
 	sendEvent(name: "windowShade", value: "open")
-    sendEvent(name: "switch",value: "on")
+    	sendEvent(name: "switch",value: "on")
 	sendEvent(name: "level", value: NVM_Target_Value, unit: "%", displayed: true)
+    	sendEvent(name: "position", value: NVM_Target_Value, unit: "%", displayed: true)
 	zwave.switchMultilevelV3.switchMultilevelSet(value: NVM_Target_Value).format()
 }
 
 def close() {
-	log.debug "close()"
+	if (logEnable) log.debug "close()"
 	Integer level = reverse ? 99 : 0
 
 	sendEvent(name: "windowShade", value: "closed")
-    sendEvent(name: "switch",value: "off")
+    	sendEvent(name: "switch",value: "off")
 	sendEvent(name: "level", value: closePosition, unit: "%", displayed: true)
+    	sendEvent(name: "position", value: closePosition, unit: "%", displayed: true)
 	zwave.switchMultilevelV3.switchMultilevelSet(value: closePosition).format()
 }
 
+def stopPositionChange() {
+    if (logEnable) log.debug "stop()"
+    if (logEnable) log.debug "$device.label stop() is not supported"
+    //zwave.switchMultilevelV3.switchMultilevelStopLevelChange().format()
+}
+
+def startPositionChange(direction) {
+    if (direction == "open") {
+        open()
+    } else {
+       close()
+    }
+}
+
 def refresh() {
-    log.debug "Refresh Tile Pushed"
+    if (logEnable) log.debug "Refresh Tile Pushed"
     delayBetween([
         zwave.switchMultilevelV1.switchMultilevelGet().format(),
         zwave.batteryV1.batteryGet().format(),
@@ -261,7 +246,7 @@ def refresh() {
 }
 
 def getBattery() {
-    log.debug  "Battery Get..."
+    if (logEnable) log.debug  "Battery Get..."
     def cmd = []
     cmd << new hubitat.device.HubAction(zwave.batteryV1.batteryGet().format())
     sendHubCommand(cmd)
@@ -291,7 +276,7 @@ def configureParams() {
     NVM_Direction_Val = boolToInteger(NVM_Direction)
     NVM_Device_Reset_Val = boolToInteger(NVM_Device_Reset_Support)
 
-	log.debug "Configuration Started"
+	if (logEnable) log.debug "Configuration Started"
     def cmds = []   
     
     // If paramater value has changed then add zwave configration command to cmds 
@@ -338,11 +323,11 @@ def configureParams() {
         cmds << zwave.configurationV1.configurationSet(parameterNumber: 11, size: 1, configurationValue: [MultiChange.toInteger()]).format()  
 	}	
     
-        log.info "Cmds: " + cmds
+        if (logEnable) log.info "Cmds: " + cmds
         commands(cmds) 
         storeParamState()
 
-    log.debug "Configuration Complete"
+    if (logEnable) log.debug "Configuration Complete"
 }
 
 private commands(cmds) { 
@@ -351,13 +336,13 @@ private commands(cmds) {
         //Use senHubCommands to fire off Z-Wave Command
         sendHubCommand(new hubitat.device.HubMultiAction(delayBetween(cmds,500), hubitat.device.Protocol.ZWAVE))
     } catch (err) {
-    log.debug "Z-Wave Parameter was not changed, therefore cmd is null and we don't need to fire off a Z-Wave command"
+    if (logEnable) log.debug "Z-Wave Parameter was not changed, therefore cmd is null and we don't need to fire off a Z-Wave command"
     }
 }
 
 
 private storeParamState() {
-    log.debug "Storing Paramater Values"
+    if (logEnable) log.debug "Storing Paramater Values"
 	state.param1 = NVM_TightLevel
     state.param2 = NVM_Direction
     state.param3 = 0  // Not used at the moment 
